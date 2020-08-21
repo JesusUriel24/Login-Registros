5416f2feaf2234668f8da119e564925ac028d637
JesusUriel24 <65378940+JesusUriel24@users.noreply.github.com>
2020-08-20 20:16:18 -0500

    Add files via upload

---
+++ Login y registros/js/index.js
@@ -0,0 +1,36 @@
+//declarar variables para vincular los objetos de formulario//
+const usuario = document.getElementById('user');
+const password = document.getElementById('pass');
+const formulario = document.getElementById('formLogin');
+
+//Generando eventos//
+formulario.addEventListener('submit', login);
+
+//funciones a ejecutar//
+function login(e) {
+    e.preventDefault();
+
+    let usuarioVal = usuario.value;
+    let passwordVal = password.value;
+
+    if(usuarioVal == '' || passwordVal == '') {
+        creaMensaje('Verifica tus campos', 'danger');
+        return;
+    }
+
+    if(localStorage.getItem('usuario')){
+        let objeto = JSON.parse(localStorage.getItem('usuario'));
+
+        if(usuarioVal == objeto.user && passwordVal == objeto.pass){
+            creaMensaje('Login Correcto', 'succes');
+            localStorage.setItem('Sesión', 'Activa');
+            setTimeout(function(){
+                window.open('./inicio.html', '_self');
+            }, 2000);
+        } else {
+            creaMensaje('Usuario Incorrecto', 'danger');
+        }
+    } else {
+        creaMensaje('No hay registros', 'danger');
+    }
+}
